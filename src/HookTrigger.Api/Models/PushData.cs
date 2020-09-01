8e6a018dac43ed3aae3232e4688de596b4c9dcfa
Mihai Dumitru <mdumitru85@gmail.com>
2020-09-01 16:29:51 +0300

    Moved PushData & Repository classes to their own files.

---
+++ src/HookTrigger.Api/Models/PushData.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HookTrigger.Api.Models
+{
+    public class PushData
+    {
+        public string[] Images { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public float PushedAt { get; set; }
+
+        public string Pusher { get; set; }
+
+        public string Tag { get; set; }
+    }
+}
---
+++ src/HookTrigger.Api/Models/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HookTrigger.Api.Models
+{
+    public class Repository
+    {
+        [JsonPropertyName("comment_count")]
+        public int CommentCount { get; set; }
+
+        [JsonPropertyName("date_created")]
+        public float DateCreated { get; set; }
+
+        public string Description { get; set; }
+
+        public string Dockerfile { get; set; }
+
+        [JsonPropertyName("full_description")]
+        public string FullDescription { get; set; }
+
+        [JsonPropertyName("is_official")]
+        public bool IsOfficial { get; set; }
+
+        [JsonPropertyName("is_private")]
+        public bool IsP5rivate { get; set; }
+
+        [JsonPropertyName("is_trusted")]
+        public bool IsTrusted { get; set; }
+
+        public string Name { get; set; }
+        public string Namespace { get; set; }
+        public string Owner { get; set; }
+
+        [JsonPropertyName("repo_name")]
+        public string RepoName { get; set; }
+
+        [JsonPropertyName("repo_url")]
+        public string RepoUrl { get; set; }
+
+        [JsonPropertyName("star_count")]
+        public int StarCount { get; set; }
+
+        public string Status { get; set; }
+    }
+}
