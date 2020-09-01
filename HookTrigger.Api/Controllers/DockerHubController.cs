fb2aa666259783975b328c8595d09e822512d1be
Mihai Dumitru <mihai-alexandru.dumitru@ing.com>
2020-09-01 11:43:41 +0300

    First implementation of DockerHubController.
    At this stage this is a pre-alpha version, currently it used to validate passing the model from body.

--- HookTrigger.Api/Controllers/DockerHubController.cs
+++ HookTrigger.Api/Controllers/DockerHubController.cs
@@ -1,8 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HookTrigger.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace HookTrigger.Api.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class DockerHubController : ControllerBase
     {
+        private readonly ILogger<DockerHubController> _logger;
+
+        public DockerHubController(ILogger<DockerHubController> logger)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost()]
+        public Task<IActionResult> CreateDockerHubPayloadAsync([FromBody] DockerHubPayload payload)
+        {
+            if (payload is null)
+            {
+                _logger.LogError($"A null {nameof(DockerHubPayload)} detected.");
+                return Task.FromResult<IActionResult>(BadRequest("An invalid payload was detected."));
+            }
+
+            return Task.FromResult<IActionResult>(Ok());
+        }
     }
 }
