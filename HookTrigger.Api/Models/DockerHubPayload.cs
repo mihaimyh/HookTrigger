d904d0e45f312458120a62ce2d401342757914d2
Mihai Dumitru <mdumitru85@gmail.com>
2020-09-01 11:42:21 +0300

    Added a model to de-serialize the Docker Hub payload.

--- HookTrigger.Api/Models/DockerHubPayload.cs
+++ HookTrigger.Api/Models/DockerHubPayload.cs
@@ -1,36 +1,73 @@
-﻿namespace HookTrigger.Api.Models
+﻿using System.Text.Json.Serialization;
+
+namespace HookTrigger.Api.Models
 {
     public class DockerHubPayload
     {
+        [JsonPropertyName("callback_url")]
         public string CallbackUrl { get; set; }
+
+        [JsonPropertyName("push_data")]
         public PushData PushData { get; set; }
+
+        //[JsonPropertyName("repository")]
         public Repository Repository { get; set; }
     }
 
     public class PushData
     {
+        //[JsonPropertyName("images")]
         public string[] Images { get; set; }
+
+        [JsonPropertyName("pushed_at")]
         public float PushedAt { get; set; }
+
+        //[JsonPropertyName("pusher")]
         public string Pusher { get; set; }
+
+        //[JsonPropertyName("tag")]
         public string Tag { get; set; }
     }
 
     public class Repository
     {
+        [JsonPropertyName("comment_count")]
         public int CommentCount { get; set; }
+
+        [JsonPropertyName("date_created")]
         public float DateCreated { get; set; }
+
+        //[JsonPropertyName("description")]
         public string Description { get; set; }
+
+        //[JsonPropertyName("dockerfile")]
         public string Dockerfile { get; set; }
+
+        [JsonPropertyName("full_description")]
         public string FullDescription { get; set; }
+
+        [JsonPropertyName("is_official")]
         public bool IsOfficial { get; set; }
+
+        [JsonPropertyName("is_private")]
         public bool IsP5rivate { get; set; }
+
+        [JsonPropertyName("is_trusted")]
         public bool IsTrusted { get; set; }
+
         public string Name { get; set; }
         public string Namespace { get; set; }
         public string Owner { get; set; }
+
+        [JsonPropertyName("repo_name")]
         public string RepoName { get; set; }
+
+        [JsonPropertyName("repo_url")]
         public string RepoUrl { get; set; }
+
+        [JsonPropertyName("star_count")]
         public int StarCount { get; set; }
+
         public string Status { get; set; }
     }
 }
