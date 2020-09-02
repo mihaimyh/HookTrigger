60fabd1a3a74e1cd5b822380e5ebe94d61f2e2a9
Mihai Dumitru <mdumitru85@gmail.com>
2020-09-02 13:12:15 +0300

    Use AspNetCore.ApiVersioning NuGet package for  Swagger and API versioning.

--- src/HookTrigger.Api/Startup.cs
+++ src/HookTrigger.Api/Startup.cs
@@ -1,5 +1,5 @@
-﻿using FluentValidation.AspNetCore;
-using HookTrigger.Api.Configuration;
+﻿using AspNetCore.ApiVersioning;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,10 +8,6 @@ using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace HookTrigger.Api
 {
@@ -34,23 +30,7 @@ namespace HookTrigger.Api
 
             app.UseHttpsRedirection();
 
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                //c.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
-                //c.RoutePrefix = swaggerOptions.RoutePrefix;
-
-                // build a swagger endpoint for each discovered API version
-                foreach (var description in provider.ApiVersionDescriptions)
-                {
-                    c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
-                    c.DocExpansion(DocExpansion.None);
-                }
-            });
+            app.UseSwaggerr(provider);
 
             app.UseRouting();
 
@@ -89,37 +69,7 @@ namespace HookTrigger.Api
                     };
                 });
 
-            services.AddApiVersioning(options =>
-            {
-                options.ReportApiVersions = true;
-            });
-
-            services.AddVersionedApiExplorer(
-                    options =>
-                    {
-                        // add the versioned API explorer, which also adds IApiVersionDescriptionProvider service
-                        // note: the specified format code will format the version as "'v'major[.minor][-status]"
-                        options.GroupNameFormat = "'v'VVV";
-
-                        // note: this option is only necessary when versioning by URL segment. the SubstitutionFormat
-                        // can also be used to control the format of the API version in route templates
-                        options.SubstituteApiVersionInUrl = true;
-                    });
-            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.OperationFilter<SwaggerDefaultValues>();
-
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
-            });
+            services.AddVersioning();
         }
     }
 }
