f2377439947ed25acb4555459fc77f143d78089a
Mihai Dumitru <mdumitru85@gmail.com>
2020-09-03 19:16:42 +0300

    Test workflow actions.

--- src/HookTrigger.Core/Models/PushData.cs
+++ src/HookTrigger.Core/Models/PushData.cs
@@ -4,6 +4,7 @@ namespace HookTrigger.Core.Models
 {
     public class PushData
     {
+        // This comment was added to test Github workflow actions.
         public string[] Images { get; set; }
 
         [JsonPropertyName("pushed_at")]
