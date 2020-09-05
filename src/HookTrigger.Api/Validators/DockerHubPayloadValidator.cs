36cd5d1a6b68c9e38fd139422173a8bf058b270c
Mihai Dumitru <mihai-alexandru.dumitru@ing.com>
2020-09-05 04:00:34 +0300

    Fixed reponame and tag validation.

--- src/HookTrigger.Api/Validators/DockerHubPayloadValidator.cs
+++ src/HookTrigger.Api/Validators/DockerHubPayloadValidator.cs
@@ -8,9 +8,9 @@ namespace HookTrigger.Api.Validators
         public DockerHubPayloadValidator()
         {
             RuleFor(p => p.PushData).Cascade(CascadeMode.Stop).NotNull();
-            RuleFor(p => p.PushData.Tag).Cascade(CascadeMode.Stop).NotEmpty().Length(2, 25);
+            RuleFor(p => p.PushData.Tag).Cascade(CascadeMode.Stop).NotEmpty().Length(2, 128);
             RuleFor(p => p.Repository).Cascade(CascadeMode.Stop).NotNull();
-            RuleFor(p => p.Repository.RepoName).Cascade(CascadeMode.Stop).NotEmpty().Length(2, 25);
+            RuleFor(p => p.Repository.RepoName).Cascade(CascadeMode.Stop).NotEmpty().Length(2, 255);
         }
     }
 }
